70ed2fc7c54e71b7df8e5e2091f6556845952f6d
Kyle <37756806+kjm1550@users.noreply.github.com>
2020-02-10 19:12:13 -0600

    The number of dice showing now change
    When a user clicks the amount of dice they want to use, the number of dice showing on the page change to reflect that amount.

--- js/script.js
+++ js/script.js
@@ -6,25 +6,39 @@ let changeNumberOfDice = number => {
 	document.getElementById(number).classList.toggle('selectItemSelected');
 };
 
+let changeNumberOfDiceShowing = number => {
+	for (x = 1; x <= 5; x++) {
+		document.getElementById('die' + x).classList.remove('itemNotUsed');
+	}
+	for (x = number + 1; x <= 5; x++) {
+		document.getElementById('die' + x).classList.add('itemNotUsed');
+	}
+};
+
 window.onload = function() {
 	document.getElementById('1').onclick = function() {
 		changeNumberOfDice(1);
+		changeNumberOfDiceShowing(1);
 	};
 
 	document.getElementById('2').onclick = function() {
 		changeNumberOfDice(2);
+		changeNumberOfDiceShowing(2);
 	};
 
 	document.getElementById('3').onclick = function() {
 		changeNumberOfDice(3);
+		changeNumberOfDiceShowing(3);
 	};
 
 	document.getElementById('4').onclick = function() {
 		changeNumberOfDice(4);
+		changeNumberOfDiceShowing(4);
 	};
 
 	document.getElementById('5').onclick = function() {
 		changeNumberOfDice(5);
+		changeNumberOfDiceShowing(5);
 	};
 
 	document.getElementById('roll').onclick = function() {};
